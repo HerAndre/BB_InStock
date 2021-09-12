0145ed4bf244e713dcfe9574845348364e1c64c6
Andre Hernandez <hernandredre@gmail.com>
2021-09-12 00:22:34 -0400

    router contains sku and postal code

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,55 +1,57 @@
 <template>
-  <div>
-    <div class="v-responsive mx-auto overflow-visible" style="max-width: 868px">
-      <div class="container container-fluid">
-        <v-form ref="form" v-model="valid" lazy-validation>
-          <v-text-field
-            v-model="postalCode"
-            :rules="rules.postalCode"
-            color="purple darken-2"
-            label="Postal Code"
-            required
-          ></v-text-field>
-          <v-text-field
-            v-model="sku"
-            :rules="rules.sku"
-            color="purple darken-2"
-            label="Sku Id"
-            required
-          ></v-text-field>
-          <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="search()"
-          >
-            Search
-          </v-btn>
-        </v-form>
-        <v-text-field disabled label="Refresh Time" v-model="lastRefresh"
-          >}</v-text-field
+  <div class="v-responsive mx-auto overflow-visible" style="max-width: 868px">
+    <div class="container container-fluid">
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          :rules="rules.postalCode"
+          color="purple darken-2"
+          label="Postal Code"
+          @change="updatePostalCode"
+          :value="postalCode"
+          required
+        ></v-text-field>
+        <v-text-field
+          :rules="rules.sku"
+          color="purple darken-2"
+          label="Sku Id"
+          @change="updateSku"
+          :value="sku"
+          required
+        ></v-text-field>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="startSearch()"
         >
-        <div class="row">
-          <div class="col">
-            <v-data-table
-              :headers="headers"
-              :items="data"
-              :items-per-page="10"
-              class="elevation-1"
-              @click:row="handleClick"
-            ></v-data-table>
-          </div>
+          Search
+        </v-btn>
+      </v-form>
+      <v-text-field
+        disabled
+        label="Refresh Time"
+        v-model="lastRefresh"
+      ></v-text-field>
+      <div class="row">
+        <div class="col">
+          <v-data-table
+            :headers="headers"
+            :items="data"
+            :items-per-page="10"
+            class="elevation-1"
+            @click:row="handleClick"
+          ></v-data-table>
         </div>
-        <v-snackbar v-model="snackbar" :color="snackbarColor" timeout="4000">
-          {{ snackbarText }}
-
-          <template v-slot:action="{ attrs }">
-            <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
-              Close
-            </v-btn>
-          </template>
-        </v-snackbar>
       </div>
+      <v-snackbar v-model="snackbar" :color="snackbarColor" timeout="4000">
+        {{ snackbarText }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
     </div>
   </div>
 </template>
@@ -62,10 +64,9 @@ export default {
   data() {
     return {
       data: [],
-      sku: "",
       valid: true,
-      postalCode: "",
       lastRefresh: null,
+      doSearch: false,
       headers: [
         {
           text: "Store name",
@@ -85,6 +86,7 @@ export default {
       },
     };
   },
+  props: ["sku", "postalCode"],
   mounted() {
     setInterval(
       function () {
@@ -94,19 +96,40 @@ export default {
     );
   },
   methods: {
+    updatePostalCode(val) {
+      this.$router.push({
+        query: { ...this.$route.query, postalCode: val },
+      });
+    },
+    updateSku(val) {
+      this.$router.push({
+        query: { ...this.$route.query, sku: val },
+      });
+    },
+    startSearch() {
+      this.doSearch = true;
+      this.search();
+    },
     async search() {
+      if (!this.doSearch) return;
+      this.snackbar = false;
+      if (!this.$refs.form.validate()) return;
       try {
-        this.snackbar = false;
-        if (!this.$refs.form.validate()) return;
         const { data } = await axios.get(`public/product/${this.sku}`, {
           params: { postalCode: this.postalCode },
         });
         this.lastRefresh = new Date();
-        this.snackbar = true;
         this.data = Object.values(data);
+        if (this.data.length > 1) {
+          this.doSearch = false;
+          window.open(
+            `https://www.bestbuy.com/site/${this.sku}.p?skuId=${this.sku}`
+          );
+        }
       } catch (error) {
         console.error(error);
       }
+      this.snackbar = true;
     },
     handleClick() {
       window.open(
