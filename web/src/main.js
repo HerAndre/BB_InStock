79ca41015d680a51a919757d9b16d5a8bddb4169
Andre Hernandez <hernandredre@gmail.com>
2022-02-02 14:25:39 -0500

    add tracking id to test google analytics

--- web/src/main.js
+++ web/src/main.js
@@ -4,10 +4,15 @@ import router from './router';
 import store from './store';
 import axios from 'axios';
 import vuetify from './plugins/vuetify';
+import VueGtag from 'vue-gtag';
 
 Vue.config.productionTip = false;
 
 axios.defaults.baseURL = '/api/';
+
+Vue.use(VueGtag, {
+  config: { id: "UA-219199426-2" }
+}, router);
 
 new Vue({
   router,
